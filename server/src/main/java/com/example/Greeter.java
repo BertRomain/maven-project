27120b7f997bd0d277f37d38b9b1aa858ea27150
BertRomain <73293890+BertRomain@users.noreply.github.com>
2021-06-18 12:00:19 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,13 +1,22 @@
 package com.example;
-
-
+/**
+ * Classe exemple pour javadoc.
+ * @author MAthilda
+ * @version 1.0
+ * @since 18/0682021
+ */
 public class Greeter {
-
-  
+/**
+* Creates an empty Greeter.
+*/
   public Greeter() {
 
   }
-
+/**
+*Methode qui renvoit "Hello babacar".
+* @param someone une personne
+* @return String une phrase
+*/
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
